668e7bf71ab27438031c2f743e65c8312d9071af
hakutrip <demkolomytsev@gmail.com>
2026-04-10 17:49:28 -0400

    Added JS, CSS
    Items on home page made with JS
    Changed CSS for items on home page
    Added link to index.html on logo
    ...

---
+++ js/items.js
@@ -0,0 +1,42 @@
+const itemsHomepage = [
+      {
+            product: {
+                  title: "Notebooks",
+                  filename: "Five Star Notebook1.jpg",
+                  price: 27.79
+            },
+            itemsLeft: 3000
+      },
+      {
+            product: {
+                  title: "Computers & Hardware",
+                  filename: "Macbook Neo.jpg",
+                  price: 599.99
+            },
+            itemsLeft: 39
+      },
+      {
+            product: {
+                  title: "Calculators",
+                  filename: "Ti-84 Plus CE Graphing Calculator.jpg",
+                  price: 195.56
+            },
+            itemsLeft: 49
+      },
+      {
+            product: {
+                  title: "Pens & Pencils",
+                  filename: "Paper Mate InkJoy Gel Pens.jpg",
+                  price: 21.99
+            },
+            itemsLeft: 7655
+      },   
+      {
+            product: {
+                  title: "Binders & Folders",
+                  filename: "3-ring Binder.jpg",
+                  price: 16.99
+            },
+            itemsLeft: 421
+      },         
+];
